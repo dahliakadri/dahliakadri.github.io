e4edc0d307bfc391e8975dd57a27462a7588dced
Dahlia Kadri <dahliakadri@gmail.com>
2020-04-03 01:38:59 +0000

    update about me

--- static/dahliadev.jsx
+++ static/dahliadev.jsx
@@ -3,7 +3,6 @@ class DahliaApp extends React.Component{
     super(props)
     this.state = { currentPage: 0,
                     pages : [<HomePage />,
-                            <About />,
                             <Projects  />,
                             <Contact />,
                             <Fun />
@@ -22,19 +21,16 @@ class DahliaApp extends React.Component{
       <div className="collapse navbar-collapse" id="collapsibleNavbar">
       <ul className="navbar-nav">
         <li className="nav-item">
-          <button className="btn btn-sm btn-outline-success" type="button" onClick={() => this.setState({currentPage: 0})}>Home</button>
+          <button className="btn btn-sm btn-outline-success" type="button" onClick={() => this.setState({currentPage: 0})}>About Me</button>
         </li>
         <li className="nav-item">
-          <button className="btn btn-sm btn-outline-secondary" type="button" onClick={() => this.setState({currentPage: 1})}>About Me</button>
+          <button className="btn btn-sm btn-outline-secondary" type="button" onClick={() => this.setState({currentPage: 1})}>Projects</button>
         </li>
         <li className="nav-item">
-          <button className="btn btn-sm btn-outline-secondary" type="button" onClick={() => this.setState({currentPage: 2})}>Projects</button>
+          <button className="btn btn-sm btn-outline-secondary" type="button" onClick={() => this.setState({currentPage: 2})}>Contact</button>
         </li>
         <li className="nav-item">
-          <button className="btn btn-sm btn-outline-secondary" type="button" onClick={() => this.setState({currentPage: 3})}>Contact</button>
-        </li>
-        <li className="nav-item">
-          <button className="btn btn-sm btn-outline-secondary" type="button" onClick={() => this.setState({currentPage: 4})}>Fun</button>
+          <button className="btn btn-sm btn-outline-secondary" type="button" onClick={() => this.setState({currentPage: 3})}>Fun</button>
         </li>
       </ul>
     </div>
@@ -52,22 +48,16 @@ class HomePage extends React.Component{
     return(
       <div className="row no-gutters justify-content-center">
       <div className="col-md-6">
-      Hi! I'm Dahlia and a full stack software engineer. I live in the San Francisco Bay Area and work in the security engineering team at Box.
-      I am originally from New York with roots in Egypt in Ireland. Previous work includes program associate for Microsoft in Cairo, Egypt, where I supported computer science programs for public school students.
-      in Egypt in Ireland. You can find my professional work, travel escapades, and my personal software/life hacks on this site.</div>
+      Hi! I'm Dahlia and a full stack software engineer. I live in the San Francisco Bay Area and work at Box.
+      I am originally from New York with roots in Alexandria, Egypt and Cork, Ireland. I currently support the Security Engineering team at Box.
+      I previously built university computer science programs at Microsoft Egypt. On this site you can find my professional work, 
+      travel escapades, and personal software/life hacks.</div>
       <div className="col-md-6">
       <img src="/static/img/dahliakadri.jpg" className="card-img" alt="Poster"/></div>
       </div>)
   }
 }
 
-class About extends React.Component{
-
-  render(){
-    return(
-      <div>"About Dahlia"</div>)
-  }
-}
 
 class Projects extends React.Component{
 
