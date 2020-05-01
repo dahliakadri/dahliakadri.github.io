b3a046668a77973a56474d7f15b8f6d9c6727c43
Dahlia Kadri <dahliakadri@gmail.com>
2020-05-01 00:40:19 +0000

    updated intro

--- static/dahliadev.jsx
+++ static/dahliadev.jsx
@@ -58,7 +58,7 @@ class HomePage extends React.Component{
       <br/>
       Hi! I'm Dahlia and a software engineer. I currently support the Security Engineering team at Box.
       I previously built university computer science programs at Microsoft Egypt. On this site you can find my professional work, 
-      travel escapades, and personal software & life hacks. </div>
+      travel escapades, and personal software & life hacks.</div>
       <div className="col-md-6">
       <img src="/static/img/dahliakadri.jpg" className="dahlia-photo" alt="Poster"/></div>
       </div>)
